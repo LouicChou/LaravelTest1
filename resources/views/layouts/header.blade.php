800d4749583e5a25bc52a8de844c4d7f2807550f
Louis <luluu0826@hotmail.com>
2024-03-10 19:06:02 +0800

    first commit

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-togger" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupporedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="#">link</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ routes/web.php
@@ -0,0 +1,73 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    $blogs = [
+        [
+            'title' => 'title one',
+            'body' => 'this is body text',
+            'status' => '1'
+        ],
+        [
+            'title' => 'title two',
+            'body' => 'this is body text',
+            'status' => '0'
+        ],
+        [
+            'title' => 'title three',
+            'body' => 'this is body text',
+            'status' => '1'
+        ],
+        [
+            'title' => 'title four',
+            'body' => 'this is body text',
+            'status' => '1'
+        ]
+    ];
+    return view('home', compact('blogs'));
+});
+
+Route::get('/about', function () {
+    $about = 'This is about page';
+    $about2 = 'This is about two';
+    return view('about',compact('about','about2'));;
+});
+
+
+Route::get('/contant', function () {
+    return view('contant.index');
+});
+
+route::group(['prefix' => 'customer'],function(){
+    Route::get('/', function () {
+        return "<h1>customer list</h1>";
+    });
+    
+    Route::get('/create', function () {
+        return "<h1>customer create</h1>";
+    });
+    
+    Route::get('/show', function () {
+        return "<h1>customer show</h1>";
+    });
+});
+
+route::fallback(function (){
+    return "Route not exist";
+});
